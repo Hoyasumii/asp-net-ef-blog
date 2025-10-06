8862e9358b7f0e1305190d5c204264d45cd735dc
Alan Reis <alanreisanjo@gmail.com>
2025-10-06 15:33:09 -0300

    Tô com Preguiça...

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -2,9 +2,17 @@ namespace Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    public int Sum(int number1, int number2)
     {
-
+        return number1 + number2;
     }
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(2, 2, 4)]
+    [InlineData(2, 3, 4)]
+    public void TestSum(int number1, int number2, int result)
+    {
+        Assert.True(Sum(number1, number2) == result);
+        }
 }
